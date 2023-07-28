eb5a2ef148083ccb51db2f4091a8fa323c5b2a98
StavWasPlayZ <stavsolomon101@gmail.com>
2023-07-28 03:19:21 +0300

    Implement voxels to keyboard

--- src/main/java/com/cstav/evenmoreinstruments/block/KeyboardBlock.java
+++ src/main/java/com/cstav/evenmoreinstruments/block/KeyboardBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -14,10 +15,21 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class KeyboardBlock extends Block {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public static final EnumProperty<KeyboardBlock.KeyboardPart> PART = EnumProperty.create("part", KeyboardPart.class);
+
+    private static final VoxelShape
+        SHAPE_LEFT_SOUTH = Block.box(0.0D, 0.0D, 4.0D, 15.65D, 4.4D, 12.8D),
+        SHAPE_RIGHT_SOUTH = Block.box(0.3D, 0.0D, 4D, 16.0D, 4.4D, 12.8D),
+
+        SHAPE_LEFT_EAST = Block.box(3.5D, 0.0D, 0.0D, 12D, 4.4D, 15.65D),
+        SHAPE_RIGHT_EAST = Block.box(3.5D, 0.0D, 0.3D, 12D, 4.4D, 16D);
+    ;
+
 
     public KeyboardBlock(Properties pProperties) {
         super(pProperties);
@@ -25,6 +37,16 @@ public class KeyboardBlock extends Block {
             .setValue(FACING, Direction.NORTH)
             .setValue(PART, KeyboardPart.LEFT)
         );
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        final Direction facing = pState.getValue(FACING);
+        final boolean southOrNorth = (facing == Direction.SOUTH) || (facing == Direction.NORTH);
+
+        return (pState.getValue(PART) == KeyboardPart.LEFT)
+            ? (southOrNorth ? SHAPE_LEFT_SOUTH : SHAPE_LEFT_EAST)
+            : (southOrNorth ? SHAPE_RIGHT_SOUTH : SHAPE_RIGHT_EAST);
     }
 
 
