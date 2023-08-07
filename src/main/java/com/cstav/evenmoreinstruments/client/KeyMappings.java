fb7fee073d5c88b6ef9a5231d54fcb7b6fb625d5
StavWasPlayZ <stavsolomon101@gmail.com>
2023-08-07 16:06:50 +0300

    Key conflict context now points to instruments

--- src/main/java/com/cstav/evenmoreinstruments/client/KeyMappings.java
+++ src/main/java/com/cstav/evenmoreinstruments/client/KeyMappings.java
@@ -3,13 +3,13 @@ package com.cstav.evenmoreinstruments.client;
 import org.lwjgl.glfw.GLFW;
 
 import com.cstav.evenmoreinstruments.Main;
+import com.cstav.genshinstrument.client.keyMaps.InstrumentKeyMappings;
 import com.mojang.blaze3d.platform.InputConstants;
 
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -22,10 +22,9 @@ public class KeyMappings {
     
     public static final Lazy<KeyMapping> VIOLIN_TYPE_MODIFIER = Lazy.of(
         () -> new KeyMapping(CATEGORY+".violin_type_modifier",
-            //TODO change to INSTRUMENT_KEY_CONFLICT
-            KeyConflictContext.GUI,
+            InstrumentKeyMappings.INSTRUMENT_KEY_CONFLICT_CONTEXT,
             InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_RIGHT_SHIFT
+            GLFW.GLFW_KEY_RIGHT_CONTROL
         , CATEGORY)
     );
 
