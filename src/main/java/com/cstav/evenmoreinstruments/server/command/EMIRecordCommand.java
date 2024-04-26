a541aa2a8946d77de283dd4b651eb1482bd0508b
StavWasPlayZ <stavsolomon101@gmail.com>
2024-04-26 21:44:28 +0300

    Remove copy command from emirecord

--- src/main/java/com/cstav/evenmoreinstruments/server/command/EMIRecordCommand.java
+++ src/main/java/com/cstav/evenmoreinstruments/server/command/EMIRecordCommand.java
@@ -1,27 +1,18 @@
 package com.cstav.evenmoreinstruments.server.command;
 
 import com.cstav.evenmoreinstruments.EMIMain;
-import com.cstav.evenmoreinstruments.item.ModItems;
 import com.cstav.evenmoreinstruments.item.emirecord.RecordRepository;
-import com.cstav.evenmoreinstruments.item.emirecord.WritableRecordItem;
-import com.cstav.evenmoreinstruments.util.CommonUtil;
 import com.mojang.blaze3d.platform.ClipboardManager;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
-import net.minecraft.client.Minecraft;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.commands.arguments.ResourceLocationArgument;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-
-import java.util.Optional;
 
 /*
 copy give to clipboard in hand
@@ -45,10 +36,6 @@ public class EMIRecordCommand {
 
     public static void register(CommandDispatcher<CommandSourceStack> pDispatcher) {
         pDispatcher.register(Commands.literal("emirecord")
-            .then(Commands.literal("copy")
-                .requires(CommandSourceStack::isPlayer)
-                .executes(EMIRecordCommand::copyRecordToClipboard)
-            )
             .then(Commands.literal("load")
                 .then(Commands.argument("name", ResourceLocationArgument.id())
                     .suggests(SUGGEST_RECORDS)
@@ -73,29 +60,6 @@ public class EMIRecordCommand {
 
     private static int saveRecordToDatapack(CommandContext<CommandSourceStack> stack) {
         stack.getSource().sendSuccess(() -> Component.literal("okie"), true);
-        return 1;
-    }
-
-    private static int copyRecordToClipboard(CommandContext<CommandSourceStack> stack) throws CommandSyntaxException {
-        final Player player = stack.getSource().getPlayer();
-        final Optional<ItemStack> record = CommonUtil.getItemInBothHands(player, ModItems.RECORD_WRITABLE.get());
-
-        if (record.isEmpty())
-            throw ERROR_NO_ITEM.create(player);
-
-        clipboard.setClipboard(
-            Minecraft.getInstance().getWindow().getWindow(),
-            String.format(
-                "/give @s %s{Channel:%s}",
-                ModItems.RECORD_WRITABLE.getId(),
-                record.get().getTagElement(WritableRecordItem.CHANNEL_TAG)
-            )
-        );
-
-        stack.getSource().sendSuccess(
-            () -> Component.translatable("commands.evenmoreinstruments.emirecord.clipboard_success"),
-            true
-        );
         return 1;
     }
 
